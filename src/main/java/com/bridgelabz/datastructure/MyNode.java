3f87052fb93aff5280a7397830b5f29713755781
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-10 13:45:25 +0530

    [Umesh]Added: welcome message

---
+++ src/main/java/com/bridgelabz/datastructure/MyNode.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure;
+
+public class MyNode<K> {
+	public static void main(String[] args) {
+		System.out.println("welcome to linked list problem");
+	}
+	
+}
---
+++ src/test/java/com/bridgelabz/datastructure/MyNodeTest.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure;
+
+import org.junit.Assert;
+import org.junit.Test;;
+
+public class MyNodeTest {
+	
+}
